f2adc78d050f89f927340f588c544cefb5b86606
Edwan Vi <edwan.vi@gmail.com>
2018-01-25 16:12:18 -0500

    Create skeleton of a resource clas

---
+++ resource.py
@@ -0,0 +1,7 @@
+class Resource():
+    def __init__(self, label, serial_no, key, value):
+        self.label = label
+        self.serial_no = serial_no
+        self.key = key
+        self.value = value
+
