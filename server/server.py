ac9b4420c2cc4de8a5843653fabb4fe8a32493f8
Edwan Vi <edwan.vi@gmail.com>
2018-01-26 12:06:43 -0500

    Use ThreadingMixIn for multithreading server-side

--- server/server.py
+++ server/server.py
@@ -1,13 +1,22 @@
 import resource
 import update
 import pickle
-import socketserver
+import socketserver, threading
+
+resources = {}
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
 class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # I CAN HANDLE MYSELF OKAY
         self.data = self.request.recv(1024).strip()
-        print("Processing data from client {}".format(self.client_address[0]))
+        print("Processing data from client {} in thread {}".format(
+                self.client_address[0],
+                threading.current_thread().name
+            )
+        )
         decoded = pickle.loads(self.data)
         print(decoded)
         r, u = None, None
@@ -18,9 +27,21 @@ class TCPHandler(socketserver.BaseRequestHandler):
             # recieved malformed dict
             print("Malformed dict from {}!".format(self.client_address[0]))
         print("Decoded pickle into update object and resource object")
+        resources[decoded[0]["label"]] = r.toDict()
+        print(resources)
 
 def main():
     print("Starting socketserver")
-    with socketserver.TCPServer(("localhost", 9999), TCPHandler) as r_server:
-        r_server.serve_forever()
-    r_server.shutdown()
+    with ThreadedTCPServer(("localhost", 9999), TCPHandler) as r_server:
+        try:
+            server_thread = threading.Thread(target=r_server.serve_forever)
+            # Exit the server thread when the main thread terminates
+            # server_thread.daemon = True
+            server_thread.start()
+            print("Server loop running in thread:", server_thread.name)
+            while True:
+                pass # HACK for Ctrl+C interrupt
+        except KeyboardInterrupt:
+            print("\nCaught Ctrl+C")
+            print("Shutting down...")
+            r_server.shutdown()
