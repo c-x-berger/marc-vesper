7786bf21dd97ac055472e14f6e9434acdf2bc7fb
Edwan Vi <edwan.vi@gmail.com>
2018-01-25 22:41:40 -0500

    Server fixes to prevent pickle errors
    There's probably some way to have the server know in advance how many bytes to read but for now I'm just after "it works"

--- server/server.py
+++ server/server.py
@@ -3,12 +3,13 @@ import update
 import pickle
 import socketserver
 
-class TCPHandler(socketserver.StreamRequestHandler):
+class TCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         # I CAN HANDLE MYSELF OKAY
-        self.data = self.rfile.readline().strip()
+        self.data = self.request.recv(1024).strip()
         print("Processing data from client {}".format(self.client_address[0]))
         decoded = pickle.loads(self.data)
+        print(decoded)
         r, u = None, None
         try:
             r = resource.Resource(decoded[0]["label"], decoded[0]["serial_no"], decoded[0]["key"], None)
@@ -22,3 +23,4 @@ def main():
     print("Starting socketserver")
     with socketserver.TCPServer(("localhost", 9999), TCPHandler) as r_server:
         r_server.serve_forever()
+    r_server.shutdown()
