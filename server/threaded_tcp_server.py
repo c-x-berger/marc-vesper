c2b8a5bb176868bc4de234ab39d033f7c0e95786
Edwan Vi <edwan.vi@gmail.com>
2018-01-29 08:08:23 -0500

    Load and unload server data via pickle

---
+++ server/threaded_tcp_server.py
@@ -0,0 +1,30 @@
+# Class for the threaded TCP socketserver and associated additions
+import socketserver
+import pickle
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True):
+        self.database = None
+        super().__init__(server_address, RequestHandlerClass, bind_and_activate)
+        print("Loading pickled database...")
+        with open("data.pickle", 'r+b') as db:
+            try:
+                self.database = pickle.load(db)
+            except FileNotFoundError:
+                print("No database file exists, loading empty DB.")
+                open("data.pickle", 'w+') # create file
+                self.database = {}
+            except (pickle.PickleError, EOFError) as e:
+                print(e)
+                print("Error unpickling data!")
+            finally:
+                print("Finished loading {} from database.".format(self.database))
+
+    def setDB(self, data):
+        self.database = data
+    
+    def server_close(self):
+        print("Closing TCP server and saving database")
+        super().server_close()
+        with open("data.pickle", 'w+b') as db:
+            pickle.dump(self.database, db)
