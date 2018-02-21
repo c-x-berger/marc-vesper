54bca7c8f99f74f2457b9fe3beee4d3afb849f97
Edwan Vi <edwan.vi@gmail.com>
2018-02-21 12:00:42 -0500

    Actually verify signature against stored key and not sent one
    Whoops.
    Flow:
    - verify against sent key
    - verify against stored key iff one is present
    - update resource object
    - update resources DB
    
    Additionally, we use a wrapper around print() everywhere to stick "[$THREADNAME]" at the start of everything.
    
    This should have been a great many commits in a nice line but I'm bad at VC.

--- server/resource.py
+++ server/resource.py
@@ -1,9 +1,9 @@
 class Resource():
-    def __init__(self, label, serial_no, key, value):
+    def __init__(self, label, serial_no, key):
         self.label = label
         self.serial_no = serial_no
         self.key = key
-        self.value = value
+        self.value = None
 
     def toDict(self):
         return {"key": self.key, "value": self.value, "serial_no": self.serial_no}
--- server/update.py
+++ server/update.py
@@ -1,29 +1,37 @@
 import nacl.signing
 import pickle
+import util
 
 class Update():
-    def __init__(self, resource, signature, value):
+    def __init__(self, resource, signature, value, oldres=None):
         self.resource = resource
         self.sig = signature
         self.value = value
+        self.oldres = oldres
 
     def check_sig(self):
-        sig_key = nacl.signing.VerifyKey(self.resource.key, encoder=nacl.encoding.HexEncoder)
+        sig_key_recv = nacl.signing.VerifyKey(self.resource.key, encoder=nacl.encoding.HexEncoder)
         try:
-            print("Trying to verify signature...")
-            sig_key.verify(self.sig)
+            util.print_labeled("Verifying data...")
+            sig_key_recv.verify(self.sig)
+            if (self.oldres != None):
+                util.print_labeled("Signature good for RECIEVED key, checking against SERVER key")
+                sig_key_serv = nacl.signing.VerifyKey(self.oldres["key"], encoder=nacl.encoding.HexEncoder)
+                util.print_labeled(self.oldres["key"])
+                sig_key_serv.verify(self.sig)
             return True
         except nacl.exceptions.BadSignatureError:
-            print("Bad signature!")
+            util.print_labeled("Bad signature!")
             return False
 
     def update_resource(self):
         if (self.check_sig()):
             # signature checks out
             # value should be an IPv6 address
-            print("Signature checks out, updating resource.")
+            util.print_labeled("Signature checks out, updating resource.")
             self.resource.value = self.value
+            return True
         else:
             # it doesn't
-            print("Passing due to signature error. No values have been updated.")
-            pass
+            util.print_labeled("Passing due to signature error. No values have been updated.")
+            return False
