a6e37d086ae29d4933baf1c67cb92d5a2c493b36
Edwan Vi <edwan.vi@gmail.com>
2018-02-21 16:54:16 -0500

    Actually fix FileNotFoundError

--- server/threaded_tcp_server.py
+++ server/threaded_tcp_server.py
@@ -2,34 +2,38 @@
 import socketserver
 import socket
 import pickle
+import util
+
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True):
         self.database = None
         print("Loading pickled database...")
-        with open("data.pickle", 'r+b') as db:
-            try:
-                self.database = pickle.load(db)
-            except FileNotFoundError:
-                print("No database file exists, loading empty DB.")
-                open("data.pickle", 'w+') # create file
-                self.database = {}
-            except Exception as e:
-                print(e)
-                print("Error unpickling data!")
-            finally:
-                print("Finished loading {} from database.".format(self.database))
+        try:
+            with open("data.pickle", 'r+b') as db:
+                try:
+                    self.setDB(pickle.load(db))
+                except Exception as e:
+                    print(e)
+                    print("Error unpickling data!")
+        except FileNotFoundError:
+            util.print_labeled("No database file exists, loading empty DB.")
+            self.setDB({})
+            with open("data.pickle", 'w+b') as db:
+                pickle.dump(self.database, db)
+        finally:
+            print("Finished loading {} from database.".format(self.database))
         self.address_family = socket.AF_INET6
         super().__init__(server_address, RequestHandlerClass, bind_and_activate)
 
     def setDB(self, data):
         self.database = data
-        
+
     def getDB(self):
         return self.database
-    
+
     def server_close(self):
-        print("Closing TCP server and saving database")
+        util.print_labeled("Closing TCP server and saving database")
         self.socket.close()
         with open("data.pickle", 'w+b') as db:
             pickle.dump(self.database, db)
