b4434a118f3f70ee45664087ba11761b2b166440
Edwan Vi <edwan.vi@gmail.com>
2018-02-22 15:35:18 -0500

    Start of actual terminal based UI

--- client/client.py
+++ client/client.py
@@ -5,11 +5,35 @@ import nacl.signing
 import datetime
 import socket
 import random
-
+import getpass
+import client_util
 
 def randomBytes(n):
     return bytes(random.getrandbits(8) for i in range(n))
 
+version = "0.0.1"
+
+def main():
+    print("marc_vesper client version {} starting...".format(version))
+    # any real startup stuff can go here in the future
+    host = input("Host: ")
+    port = int(input("Port: "))
+    family = None
+    if (client_util.query_yes_no("IPv6?")):
+        family = socket.AF_INET6
+    else:
+        family = socket.AF_INET
+    print("Creating connection to {}/{}...".format(host, port))
+    with socket.socket(family, socket.SOCK_STREAM) as sock:
+        sock.connect((host, port))
+        print("Connected to {}/{}!".format(host, port))
+        pass_phrase = getpass.getpass("Please enter your password for {}: ".format(host))
+        random.seed(pass_phrase)
+        signing_key - nacl.signing.SigningKey(randomBytes(32), nacl.encoding.RawEncoder)
+
+if __name__ == "__main__":
+    main()
+    exit(0)
 
 # Generate a new random signing key
 random.seed(sys.argv[2])
@@ -45,3 +69,4 @@ with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as sock:
 
     # Receive data from the server and shut down
     received = str(sock.recv(1024), "utf-8")
+
