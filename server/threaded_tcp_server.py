ad31e46c239f72e20a042140bdb84ddb0b84bf79
Edwan Vi <edwan.vi@gmail.com>
2018-02-22 15:33:13 -0500

    Label more prints

--- server/threaded_tcp_server.py
+++ server/threaded_tcp_server.py
@@ -2,27 +2,28 @@
 import socketserver
 import socket
 import pickle
+import threading
 import util
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     def __init__(self, server_address, RequestHandlerClass, bind_and_activate=True):
         self.database = None
-        print("Loading pickled database...")
+        util.print_labeled("Loading pickled database...")
         try:
             with open("data.pickle", 'r+b') as db:
                 try:
                     self.setDB(pickle.load(db))
                 except Exception as e:
                     print(e)
-                    print("Error unpickling data!")
+                    util.print_labeled("Error unpickling data!")
         except FileNotFoundError:
             util.print_labeled("No database file exists, loading empty DB.")
             self.setDB({})
             with open("data.pickle", 'w+b') as db:
                 pickle.dump(self.database, db)
         finally:
-            print("Finished loading {} from database.".format(self.database))
+            print("[{}] Finished loading {} from database.".format(threading.current_thread().name, self.database))
         self.address_family = socket.AF_INET6
         super().__init__(server_address, RequestHandlerClass, bind_and_activate)
 
