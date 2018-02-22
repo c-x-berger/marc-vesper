1a5fca14fc226b6a9cb29247ead944f6d17a2f23
Edwan Vi <edwan.vi@gmail.com>
2018-02-22 15:46:15 -0500

    Generate key from input seed
    TODO: rest of UI, I guess

--- client/client.py
+++ client/client.py
@@ -35,6 +35,19 @@ def main():
         random.seed(pass_phrase)
         signing_key - \
             nacl.signing.SigningKey(randomBytes(32), nacl.encoding.RawEncoder)
+        verify_key = signing_key.verify_key
+        verify_key_hex = verify_key.encode(encoder=nacl.encoding.HexEncoder)
+        print("Generated signing key and verify key successfully!")
+
+        current_time = datetime.datetime.now(datetime.timezone.utc)
+        unix_time = current_time.timestamp()
+        # TODO: Get input for all updates
+        update = [{"label": "test.hype", "serial_no": unix_time, "key": verify_key_hex,
+                   "value": "fcc3:a9d9:1694:2d:7a61:b5af:95fb:85d6"}, None]
+        pickled_update = pickle.dumps(update[0])
+        # Sign a message with the signing key
+        signed = signing_key.sign(pickled_update)
+        update[1] = signed
 
 
 if __name__ == "__main__":
