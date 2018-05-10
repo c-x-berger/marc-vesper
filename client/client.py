fd0b7b449462c718452f47989f2e78b4097a6cd6
Edwan Vi <edwan.vi@gmail.com>
2018-05-10 15:47:03 -0400

    Expand client.py to support more than one kind of message

--- client/client.py
+++ client/client.py
@@ -5,6 +5,7 @@ import nacl.signing
 import datetime
 import socket
 import random
+import time
 import getpass
 import client_util
 
@@ -41,20 +42,28 @@ def main():
             encoder=nacl.encoding.URLSafeBase64Encoder), "utf-8")
         print("(Re)generated signing key and verify key successfully!")
         # label, value, serial number
-        label = input("Resource to update (label): ")
-        value = input("Value to set resource to: ")
-        current_time = datetime.datetime.now(datetime.timezone.utc)
-        unix_time = current_time.timestamp()
-        # update "object"
-        update = [{"label": label, "serial_no": unix_time,
-                   "key": verify_key_b64, "value": value}, None, 100]
-        json_update = json.dumps(update[0])
-        # Sign a message with the signing key
-        signed = str(signing_key.sign(bytes(json_update, "utf-8"),
-                                      nacl.encoding.URLSafeBase64Encoder), "utf-8")
-        update[1] = signed
-        print(json.dumps(update))
-        sock.sendall(bytes(json.dumps(update), "utf-8"))
+        r_type = int(input("Type of request: "))
+        label = input("Label: ")
+        if (r_type == 100):
+            value = input("Value to set resource to: ")
+            current_time = datetime.datetime.now(datetime.timezone.utc)
+            unix_time = current_time.timestamp()
+            # update "object"
+            update = [{"label": label, "serial_no": unix_time,
+                       "key": verify_key_b64, "value": value}, None, 100]
+            json_update = json.dumps(update[0])
+            # Sign a message with the signing key
+            signed = str(signing_key.sign(bytes(json_update, "utf-8"),
+                                          nacl.encoding.URLSafeBase64Encoder), "utf-8")
+            update[1] = signed
+            print(json.dumps(update))
+            sock.sendall(bytes(json.dumps(update), "utf-8"))
+        elif (r_type == 200):
+            data = [label, "EMPTY", 200]
+            json_request = json.dumps(data)
+            print(json_request)
+            sock.sendall(bytes(json_request, "utf-8"))
+            print(str(sock.recv(1024), "utf-8"))
 
 
 if __name__ == "__main__":
